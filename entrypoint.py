341b84396cb32207ad81f8f72f4899068f770d53
Max <maxwell.rubin.willard@gmail.com>
2020-05-22 19:57:44 -0700

    Made nicer print statement.

--- entrypoint.py
+++ entrypoint.py
@@ -4,7 +4,5 @@ sc = pyspark.SparkContext('local[*]')
 
 txt = sc.textFile('file:////usr//share//X11//Xcms.txt')
 
-print(txt.count())
-
 python_lines = txt.filter(lambda line: 'the' in line.lower())
-print(python_lines.count())
+print("The number of lines containing 'the' in your file is: ", python_lines.count())
